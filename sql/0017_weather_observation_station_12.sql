166e314b6adaa9691d265453cf9d2773c32f9a41
bsoares00 <bruno.ed@gmail.com>
2021-08-06 12:17:02 +0200

    SQL challenges 15-23

---
+++ sql/0017_weather_observation_station_12.sql
@@ -0,0 +1,9 @@
+/*
+
+Query the list of CITY names from STATION that do not start with vowels and
+do not end with vowels. Your result cannot contain duplicates.
+
+
+*/
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(CITY) NOT LIKE '[aeiou]%' AND LOWER(CITY) NOT LIKE '%[aeiou]';
+
